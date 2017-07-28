195cf90df9dffb58029d33efe0cba5f5bb8a3295
sharbatc <sharbatanu444@gmail.com>
2017-07-28 12:04:18 +0200

    added generation of artificial flow fields

--- optic_flow.py
+++ optic_flow.py
@@ -57,3 +57,41 @@ def optic_flow(stimulus):
     v_x, v_y = mt_cell.get_optic_flow_combined()
     
     return v_x, v_y
+
+## This one is used to generate some random optic flows for getting the mmonocular ego motion extraction to work. 
+def calculate_A(f,x,y):
+    A = np.array([[-f,0,x],[0,-f,y]])
+    assert A.shape == (2,3), "Wrong shape of A(x,y) matrix, should be {} but is {}".format((2,3),A.shape)
+    return A
+
+def calculate_B(f,x,y):
+    B = np.array([[(x*y)/f, -(f + (x*x)/f), y],[f + (y*y)/f, -(x*y)/f, -x]])
+    assert B.shape == (2,3), "Wrong shape of B(x,y) matrix, should be {} but is {}".format((2,3),B.shape)
+    return B
+
+# Will generate from arbitrary depth and given translation and rotation, a optic flow field at (x,y) 
+def get_point_optic_flow(f,x,y,T,Omega):
+    assert T.shape == (3,1), "Wrong shape of T matrix, should be {} but is".format((3,1),T.shape)
+    assert Omega.shape == (3,1), "Wrong shape of Omega matrix, should be {} but is".format((3,1),Omega.shape)
+
+    Z = np.random.rand() #depth, arbitrary
+    A_xy = calculate_A(f,x,y)
+    B_xy = calculate_B(f,x,y)
+
+    translation = (1/Z)*np.dot(A_xy,T)
+    rotation = np.dot(B_xy,Omega)
+
+    v_xy = translation + rotation 
+    assert v_xy.shape == (2,1), "Wrong shape of velocity matrix, should be {} but is".format((2,1),v_xy.shape)
+    return v_xy
+
+# Will generate the whole optic flow field for a scene size of (X,Y) pixels #can and should be parallelised 
+def get_artificial_optic_flow(f,X,Y,T,Omega):
+    assert T.shape == (3,1), "Wrong shape of T matrix, should be {} but is".format((3,1),T.shape)
+    assert Omega.shape == (3,1), "Wrong shape of Omega matrix, should be {} but is".format((3,1),Omega.shape)
+
+    O_field = np.zeros((X,Y))
+
+    for i in np.arange(0,X,1):
+        for j in np.arange(0,Y,1):
+            O_field(i,j) = get_point_optic_flow(f,i,j,T,Omega)
